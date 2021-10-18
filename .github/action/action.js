31c141f6f71015ae2b2d52409735badda06a7290
Sam Donald <32623552+mudlabs@users.noreply.github.com>
2021-10-19 10:38:47 +1100

    Update action.js

--- .github/action/action.js
+++ .github/action/action.js
@@ -1,5 +1,30 @@
 const fs = require("fs");
 
+// A user had sumbitted a widget PR and it has been accepted.
+
+
+// Required
+// Extract front matter
+// Expected output:
+// {
+//   name: The name of the widget
+//   summary: Brief description of the widget
+// }
+// function getFrontMatter(file) {}
+
+
+// Required
+// Extracts the <body>...</body> of the new widget.
+// function getBody(file) {}
+
+
+// Extracts the <style>...</style> of the new widget.
+// function getStyle(fale) {}
+
+
+// Extracts the <script>...</script> of the new widget.
+// function getScript(file) {}
+
 
 
 
