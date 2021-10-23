a76d18b221ac9a53cd0f988081d5942b14bb609c
Sam Donald <32623552+mudlabs@users.noreply.github.com>
2021-10-23 15:13:24 +1100

    Update action.js

--- .github/action/action.js
+++ .github/action/action.js
@@ -81,8 +81,7 @@ async function addContributor(template, engine, data) {
   const readme = await fs.promises.readFile("./README.md", fs_options);
   const cheerio_instance = cheerio.load(readme);
   const contributors = cheerio_instance("#contributors");
-  console.log(cheerio_instance.html(), contributors)
-  if (!contributors(`#${data.author}`)) {
+  if (contributors.has(`#${data.author}`).length === 1) {
     const _engine = await engine.parse(template);
     const td = _engine.render(data);
     const last_tr = contributors.last("tr");
