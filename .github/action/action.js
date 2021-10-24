43f47bc7e3fa6018a37f9ed9c981b2e404c2e5fb
Sam Donald <32623552+mudlabs@users.noreply.github.com>
2021-10-25 09:47:47 +1100

    Update action.js

--- .github/action/action.js
+++ .github/action/action.js
@@ -7,39 +7,42 @@ const core = require("@actions/core");
 const github = require("@actions/github");
 const octo = require("@octokit/core");
 
-const fs_options = { encoding: "utf-8" };
+
 const owner = "mudlabs";
 const repo = "web-widgets";
-
+const fs_options = { encoding: "utf-8" };
 const octokit = github.getOctokit(core.getInput("token"));
 
-// A user had sumbitted a widget PR and it has been accepted.
-// const async hasUniqueName = () => {
-//   const file = await fs.promises.readFile("./docs/index.html", fs_options);
-//   const cheerio_instance = cheerio.load(file);
-//   const widgets = cheerio_instance("#widgets");
-//   return widgets.has(``).length === 1;
-// }
+const readFile = async (path, options = fs_options) => await fs.promises.readFile(path, options);
+const writeFile = async (path, contents, options = fs_options) => await fs.promises.writeFile(path, contents, options);
+const cheerify = file => cheerio.load(file);
+const widgetId = (author, name) => `${author}--${name.replace(/\s+/g, "").toLowerCase()}`;
+
+// Finds the original author of a file created on the repo.
+async function originalAuthor(path) {
+  const commits = await octokit.request("GET /repos/{owner}/{repo}/commits", { owner, repo, path, sha: "dev"});
+  const original = commits.data.pop();
+  const author = original.author.login;
+  return { author };
+}
 
 // Request widget file from commit and return its contents as a cheerio instance.
-async function getWidgetFile(file_name) {
+async function getWidgetFile(path) {
   const commits = github.context.payload.commits;
   const commit = await octokit.request({
           method: "GET",
           url: `https://api.github.com/repos/mudlabs/web-widgets/commits/${commits[0].id}`
         });
   const commit_file = commit.data.files[0];
-  const f = await octokit.request("GET /repos/{owner}/{repo}/commits", {owner:"mudlabs",repo: "web-widgets", path: file_name, sha: "dev"});
-  f.data.forEach(i => console.log(i.author, i.committer));
-  const path = file_name.replace(/^widgets\//, "");
-  const file = await fs.promises.readFile(file_name, fs_options);
+  const name = path.replace(/^widgets\//, "");
+  const file = await fs.promises.readFile(path, fs_options);
   const context = cheerio.load(file);
-  return { path, context };
+  return { name, context };
 }
 
 
 // Required
-function getRenderData(cheerio_instance) {
+async function getRenderData(cheerio_instance) {
   const validate = item => typeof item == "string" && item !== "";
   const date = new Date().toDateString();
   const name = cheerio_instance("body").data("widget-name");
@@ -62,64 +65,81 @@ function getWidget(cheerio_instance) {
 }
 
 async function loadTemplates() {
-  const item = await fs.promises.readFile("./.github/action/templates/item.html", fs_options);
-  const widget = await fs.promises.readFile("./.github/action/templates/widget.html", fs_options);
-  const contributor = await fs.promises.readFile("./.github/action/templates/contributor.html", fs_options);
+  const item = await readFile("./.github/action/templates/item.html");
+  const widget = await readFile("./.github/action/templates/widget.html");
+  const contributor = await readFile("./.github/action/templates/contributor.html");
   
   return { item, widget, contributor };
 }
 
-async function prependItemToList(item) {
-  const file = await fs.promises.readFile("./docs/index.html", fs_options);
-  const context = cheerio.load(file);
-  context(".widgets").prepend(item);
-  return context.html();
-}
-
-async function writeHomePage(page) {
-  const result = await fs.promises.writeFile("./docs/index.html", page, fs_options);
-  return result;
+async function updateList(item) {
+  const path = "./docs/index.html";
+  const file = await readFile(path);
+  const instance = cheerify(file);
+  const page = instance(".widgets").prepend(item);
+  const write = await writeFile(path, page);
+  return write;
 }
 
 async function writeWidget(path, widget) {
-  const result = await fs.promises.writeFile(`./docs/${path}`, widget, fs_options);
+  const result = await writeFile(`./docs/${path}`, widget);
   return result;
+}
+
+
+async function removeContributor() {
+  const instance = cheerify(list);
+  const hasWidgets = instance(`li[class=widget][data-author=${author}`).length === 0 ? false : true;
+  if (!hasWidgets) {
+    const readme = await readFile("./README.me");
+    const instance = cheerify(readme);
+    const instance(`td[id=${author}`).remove();
+    const result = await writeFile("./README.md", instance.html());
+    return result;
+  }
+  return;
 }
 
 async function addContributor(template, engine, data) {
   let changed = false;
-  const readme = await fs.promises.readFile("./README.md", fs_options);
-  const cheerio_instance = cheerio.load(readme);
-  const contributors = cheerio_instance("#contributors");
+  const readme = await readFile("./README.md");
+  const instance = cheerify(readme);
+  const contributors = instance("#contributors");
   if (contributors.has(`#${data.author}`).length === 0) {
-    const _engine = await engine.parse(template);
-    const td = await _engine.render(data);
+    const readme_engine = await engine.parse(template);
+    const td = await readme_engine.render(data);
     const last_tr = contributors.children("tr").last();
     last_tr.children("td").length > 6
       ? contributors.append(`<tr>${td}</tr>`)
       : last_tr.append(td);
-    changed = true;
-  }
-  
-  return { changed, contents: cheerio_instance.html() };
-}
-
-async function writeReadme(file) {
-  if (file.changed) {
-    const result = await fs.promises.writeFile("./README.md", file.contents, fs_options);
+    const result = await writeFile("./README.md", instance.html());
     return result;
-  } else {
-    return;
   }
+  return;
 }
 
+// Runs if a commit has deleted a widget.
+async function removedWidget(author, name, file) {
+  const list = await readFile("widgets/index.html");
+  const instance = cheerify(list);
+  const widgets = instance("#widgets");
+  const id = widgetID(author, name);
+  const item widgets.children(`#${id}`);
+  if (item.length === 1) {
+    instance(item).remove();
+    const rm = await fs.promises.unlink(`docs/${file}`);
+    core.setOutput("author", author);
+    return `SUCCESS: ${file} widget removed.`;
+  } else {
+    throw Error(`
+      Could not remove widget (${data.name}). Expected child count 1, found ${child.length}.
+        -  ID: ${id}
+    `);
+  }
+}
 
 (async function(){
   try {
-    console.log(github.context.payload);
-    console.log("--------------------------");
-    console.log(github.context.payload.commits);
-    return;
     const added = core.getInput("added");
     const removed = core.getInput("deleted");
     const modified = core.getInput("modified");
@@ -128,21 +148,32 @@ async function writeReadme(file) {
     const previous = core.getInput("previous");
     
     const file = await getWidgetFile(name);
+    const original = await originalAuthor(name);
     const data = getRenderData(file.context);
-    return
+    const templates = await loadTemplates();
+    const engine = new liquid.Engine();
+    
     
     if (removed) {
-      // was it removed by its author?
-      // delete from list
-      // delete from /docs
-      // setOutput widget_author
-    } else if (added) {
-      // add to list
-      // add to /docs
-      // setOutput widget_author
+      const done = await removedWidget(original.author, data.name, file.name);
+      const result = await removeContributor()
+      return result;
     }
-    return
     
+    if (data.invalid) throw `There is a problem with the widgets required data attributes. Please check the documentation.`;
+    
+    data["widget"] = getWidget(file.context);
+    data["path"] = file.name;
+    
+    const engine = new liquid.Engine();
+    const widget_engine = await engine.parse(templates.widget);
+    const item_engine = await engine.parse(templates.item)
+    
+    const widget = await widget_engine.render(data);
+    const item = await item_engine.render(data);
+    
+    const list = await updateList(item)
+    const page = await writeWidget(file.name, widget);
     // if a widget was created
       // add it to the list
       // add it to /docs
@@ -150,21 +181,7 @@ async function writeReadme(file) {
     // if a widget was changed 
       // update list
       // update in /docs
-    
-    if (data.invalid) throw `The document does not contain valid data-widget-name or data-widget-summary fields. These should be attributes of the <body> tag.`;
-    data["widget"] = getWidget(file.context);
-    data["path"] = file.path;
-    const templates = await loadTemplates();
-    const engine = new liquid.Engine();
-    const widget_engine = await engine.parse(templates.widget);
-    const widget = await widget_engine.render(data);
-    const widget_written = await writeWidget(file.path, widget)
-    const item_engine = await engine.parse(templates.item)
-    const item = await item_engine.render(data);
-    const page = await prependItemToList(item);
-    const writePage = await writeHomePage(page);
-    const readme = await addContributor(templates.contributor, engine, data);
-    const write_readme = await writeReadme(readme);
+    if (added) await addContributor(templates.contributor, engine, data);
     return "Success";
   } catch(error) {
     core.setFailed(error);
